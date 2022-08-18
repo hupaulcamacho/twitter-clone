384095b0f22bbae1ef943fda94544951608f1203
Hupaul <hupaulcamacho@pursuit.org>
2022-08-18 12:29:39 -0400

    initialize boilerplate react app and clean out unnecessary files

---
+++ twt-clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <h1>Hey Clever Programmers... lets build a twitter clone</h1>
+    </div>
+  );
+}
+
+export default App;
